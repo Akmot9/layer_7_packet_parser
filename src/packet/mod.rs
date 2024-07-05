894495e38676bb0da0eeab3e4dc837bce463bff5
akmot9 <avicocyprien@yahoo.com>
2024-07-05 09:48:55 +0200

    better crate docs

--- src/packet/mod.rs
+++ src/packet/mod.rs
@@ -1,4 +1,16 @@
-//! Packet module containing submodules for different packet types.
+//! # Packet Module
+//!
+//! This module contains submodules for different packet types, each providing functionality for parsing and representing the specific protocol's packets.
+//!
+//! ## Submodules
+//!
+//! - [`bitcoin`]: Contains functions and structures for parsing Bitcoin protocol packets.
+//! - [`dhcp`]: Contains functions and structures for parsing DHCP protocol packets.
+//! - [`dns`]: Contains functions and structures for parsing DNS protocol packets.
+//! - [`http`]: Contains functions and structures for parsing HTTP protocol requests.
+//! - [`modbus`]: Contains functions and structures for parsing Modbus protocol packets.
+//! - [`ntp`]: Contains functions and structures for parsing NTP protocol packets.
+//! - [`tls`]: Contains functions and structures for parsing TLS protocol packets.
 
 pub mod bitcoin;
 pub mod dhcp;
